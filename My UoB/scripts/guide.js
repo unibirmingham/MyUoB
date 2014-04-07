600baf2616f74e37b2fd1d11a804d876fb269e47
Jon Winterbourn <j.p.winterbourn@bham.ac.uk>
2014-04-07 07:49:20 -0500

    fix auto pocket guide

--- My UoB/scripts/guide.js
+++ My UoB/scripts/guide.js
@@ -10,18 +10,9 @@
     }
     
     guide = new kendo.data.DataSource({
-    	/*
-        transport: { 
-        	read: { 
-            	url: guideUrl,
-                dataType: "json"
-            }
-        }
-        */
 		transport: {
             read: function(operation) {
 	            var cachedData = localStorage.getItem("pocketGuide");
-				//var cachedDate = localStorage.getItem("lastCached");
             	if((cachedData != null || cachedData != undefined) && (!navigator.onLine)) {
                 	//if local data exists and we're offline, load from it
                 	operation.success(JSON.parse(cachedData));
@@ -45,63 +36,32 @@
         
 })(window);
 
+
+
 function guideShow(e) {
-    //var myFilter = { logic: "and", filters: [{field: "PageFunction", operator: "eq", value: "index"},{field: "ParentId", operator: "eq", value: 1}] };
     var myFilter = { logic: "and", filters: [{field: "ParentId", operator: "eq", value: 1}] };
     guide.filter(myFilter);
-    //app.application.navigate("#tabstrip-pocket-guide2");
-};
-
-function guideSectionShow(e) {
-    //alert(e.dataItem);
-    //var id = e.dataItem.id;
-    //guide.filter({field: "ParentId", operator: "eq", value: id});
 };
 
 function onGuideIndexClick(e) {
-    var id = e.dataItem.id;
-    var name = e.dataItem.PageTitle;
-       
-    if (e.dataItem.PageFunction === "page") {
-        guide.filter({field: "id", operator: "eq", value: id});
-        app.application.navigate("#guide-page-feedback");    
-    }
-    else {
-        guide.filter({field: "ParentId", operator: "eq", value: id});
-        app.application.navigate("#guide-section");
-    }
+
+};
+
+function guideSectionShow(e) {
+    id = parseInt(e.view.params.id);
+    guide.filter({field: "ParentId", operator: "eq", value: id});
 };
 
 function onGuideSectionClick(e) {
-    var id = e.dataItem.id;
-    var name = e.dataItem.PageTitle;
-    var parent = e.dataItem.ParentId;
-    guide.filter({field: "id", operator: "eq", value: id});
     
-    app.application.navigate("#guide-page");
-    $('#backFromPage').attr('data-parent', parent);
-    $('#backFromPage').attr('href','#guide-section?id=' + parent);
 };
-
-function onBackFromPage(e) {
-    //onGuideSectionClick(e);
-    
-    //var id =e.id;
-  //  id = this.element.attr("data-parent")
-//    alert("boom" + id);
- //   guide.filter({field: "ParentId", operator: "eq", value: id});   
-    //app.application.navigate("#guide-section");
-   
-}
-
-function onGuidePageClick(e) {
-   var parent = e.dataItem.ParentId;
-   //alert(parent);
-   guide.filter({field: "ParentId", operator: "eq", value: parent});   
-   app.application.navigate("#guide-section");
-}
-
 
 function guidePageShow(e) {
+    id = parseInt(e.view.params.id);
+    guide.filter({field: "id", operator: "eq", value: id});
+};
+
+function onPageSectionClick(e) {
     
 };
+
